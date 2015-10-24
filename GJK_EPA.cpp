ddfa8b332712b4e646f1d201400a8e22317e1b61
laneboy <lucasrivera4@aol.com>
2015-10-24 14:24:35 -0400

    Update GJK_EPA.cpp

--- GJK_EPA.cpp
+++ GJK_EPA.cpp
@@ -21,25 +21,51 @@ bool SteerLib::GJK_EPA::intersect(float& return_penetration_depth, Util::Vector&
 //GJK Algorithm Following syntax form given in GJK_EPA.h
 struct GJK{//I don't belive in returing things through arguments that is bad programing practice
 	bool collision;
-	void* simplex;//not sure what this should be yet
+	Polygon simplex;//Iteritve Polygon that is contained in the Minkoski difference and containes the origen
 };typedef struct* GJK GJK_Object;
 ;typedef const std::vector<Util::Vector>& Polygon;
+;typedef const std::vector<int>& Point;
 GJK_Object SteerLib::GJK_EPA::GJK(Polygon A, Polygon B){
 	GJK_Object toRet = (GJK_Object)malloc(sizeof(struct GJK));
-	toRet->collision = false;
-	toRet->simplex = NULL;
-	if (A collides with B){//SUDO CODE
-		toRet->collision = true;
-        	//return (Simplex, true);
-		return toRet;
+	Polygon simplex = getSimplex(A,B);
+	if (!simplex){//if the simplex is not null
+		if(containsOrigen(simplex)){//does the simplex contain the origen if so => collision
+        		toRet->collision = true;
+			toRet->simplex = simplex;
+			return toRet;
+		}
+		else{
+			toRet->collsion = false;
+			free(toRet->simplex);
+			toRet->simplex = NULL;
+			return toRet;
+		}
 	}
         else{
+		
+		toRet->collision = false;
+		toRet->simplex = NULL;
         	//return (NULL, false);
 		return toRet;
 	}
 }
+Point support(Polygon s1, Polygon s2,const std::vector d){
+	Point p1 = getPointFromDirection(s1,d);
+	Point p2 = getPointFromDirection(s2,inverse(d));
+	Point p3 = pointSubtraction(p1,p2);
+	return p3;
+}
+Point getPointFromDirection(Polygon s, const std::vector d){
+	
+	return
+}
+Point pointSubtraction(Point p1, Point p2){
 
+}
+Polygon getSimplex(Polygon A,Polygon B){
 
+}
+bool containesOrigen(Polygon P){
 
-
+}
 
